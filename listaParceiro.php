b0630077f8a690b6a0953cb090e28cc660b1b2ef
Dinho <dinhogugu_@hotmail.com>
2020-12-22 00:28:34 -0300

    commitac

--- listaParceiro.php
+++ listaParceiro.php
@@ -13,8 +13,6 @@ $c = new Cadastroip;
      <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/cssinterno.css">
-    <link rel="stylesheet" href="assets/css/demo.css">
     <link rel="stylesheet" href="cssphp/estilocssAP.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
